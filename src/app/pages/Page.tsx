384b654586b003bfcbc5177fc8eb5d370962bd26
st-thule <thu.le@supremetech.vn>
2025-04-20 23:42:41 +0700

    feat: add home into app

--- src/app/pages/Page.tsx
+++ src/app/pages/Page.tsx
@@ -1,11 +1,13 @@
-import { Header } from "@shared/components/layout/Header";
-
 import React from "react";
+
+import { Header } from "@shared/components/layout/Header";
+import { Home } from "@app/pages/home/Home";
 
 export const Page = () => {
   return (
     <>
       <Header />
+      <Home />
     </>
   );
 };
