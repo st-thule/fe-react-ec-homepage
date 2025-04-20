3512fc3e094b07b433cd1c9fa5b0a1f758f1a447
st-thule <thu.le@supremetech.vn>
2025-04-20 16:44:23 +0700

    refactor: change button component

--- src/app/shared/components/partials/Button.tsx
+++ src/app/shared/components/partials/Button.tsx
@@ -3,11 +3,6 @@ import React from "react";
 interface ButtonProps {
   className?: string;
   onClick?: () => void;
-  type: "primary" | "secondary";
-  size?: "sm" | "xl";
-  style?: "normal" | "bold";
-  isFlex?: boolean;
-  isFontSmall?: boolean;
   label: string;
   icon?: string;
 }
@@ -15,26 +10,11 @@ interface ButtonProps {
 const Button: React.FC<ButtonProps> = ({
   className = "",
   onClick,
-  type = "primary",
-  size = "xl",
-  style = "normal",
-  isFlex = false,
-  isFontSmall = false,
   label,
   icon,
 }) => {
-  const buttonClass = `
-    btn
-    ${type === "primary" ? "btn-primary" : "btn-secondary"}
-    ${size === "sm" ? "btn-sm" : "btn-xl"}
-    ${style === "bold" ? "btn-bold" : ""}
-    ${isFlex ? "btn-flex" : ""}
-    ${isFontSmall ? "btn-font-small" : ""}
-    ${className}
-  `.trim();
-
   return (
-    <a className={buttonClass} onClick={onClick}>
+    <a className={`btn ${className}`} onClick={onClick}>
       {icon && (
         <span className="btn-icon">
           <img src={icon} alt="button icon" />
