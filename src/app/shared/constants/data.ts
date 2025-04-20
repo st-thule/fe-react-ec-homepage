73e5fce2f1dc31bcb014ed9f4c1123dded87f6af
st-thule <thu.le@supremetech.vn>
2025-04-20 23:42:55 +0700

    feat: add seller list constant

--- src/app/shared/constants/data.ts
+++ src/app/shared/constants/data.ts
@@ -26,3 +26,41 @@ export const NavItems: NavItemProps[] = [
     href: "/contact",
   },
 ];
+
+export const sellerItem = [
+  {
+    id: 1,
+    alt: "Sheba",
+    image: "assets/images/sellers/sheba.png",
+  },
+  {
+    id: 2,
+    alt: "Whiskas",
+    image: "assets/images/sellers/whiskas.png",
+  },
+  {
+    id: 3,
+    alt: "Bakers",
+    image: "assets/images/sellers/bakers.png",
+  },
+  {
+    id: 4,
+    alt: "Felix",
+    image: "assets/images/sellers/felix.png",
+  },
+  {
+    id: 5,
+    alt: "Good Boy",
+    image: "assets/images/sellers/goodboy.png",
+  },
+  {
+    id: 6,
+    alt: "Butchers",
+    image: "assets/images/sellers/butchers.png",
+  },
+  {
+    id: 1,
+    alt: "Pedigree",
+    image: "assets/images/sellers/pedigree.png",
+  },
+];
