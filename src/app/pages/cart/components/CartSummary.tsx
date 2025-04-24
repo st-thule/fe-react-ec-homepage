695f486899930a79af49031d1c0a6ec12e681873
st-thule <thu.le@supremetech.vn>
2025-04-24 16:28:12 +0700

    feat: style for cart summary

--- src/app/pages/cart/components/CartSummary.tsx
+++ src/app/pages/cart/components/CartSummary.tsx
@@ -15,15 +15,15 @@ export const CartSummary: React.FC<ICartSummaryProps> = ({
       <div className="cart-info">
         <div className="cart-detail">
           <h3 className="cart-label">Quantity</h3>
-          <p className="cart-value">{totalPrice}</p>
+          <p className="cart-value">{totalQuantity}</p>
         </div>
         <div className="cart-detail">
           <h3 className="cart-label">Total</h3>
-          <p className="cart-value">{totalQuantity} VND</p>
+          <p className="cart-value">{totalPrice} VND</p>
         </div>
       </div>
       <div className="cart-action">
-        <Button label={'Checkout'} className="" />
+        <Button label={'Checkout'} className="btn btn-secondary" />
       </div>
     </div>
   );
