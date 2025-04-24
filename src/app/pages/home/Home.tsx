41a5974fab1b9e3cabe4bec52b85d93032851868
st-thule <thu.le@supremetech.vn>
2025-04-24 23:27:09 +0700

    refactor: move page into components

--- src/app/pages/home/Home.tsx
+++ src/app/pages/home/Home.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-import { HeroBanner } from "@app/pages/home/container/HeroBanner";
-import { FriendBanner } from "@app/pages/home/container/FriendBanner";
-import { Sellers } from "@app/pages/home/container/Sellers";
-import { Adoption } from "@app/pages/home/container/Adoption";
+import { HeroBanner } from "@app/pages/home/components/HeroBanner";
+import { FriendBanner } from "@app/pages/home/components/FriendBanner";
+import { Sellers } from "@app/pages/home/components/Sellers";
+import { Adoption } from "@app/pages/home/components/Adoption";
 
 export const Home = () => {
   return (
