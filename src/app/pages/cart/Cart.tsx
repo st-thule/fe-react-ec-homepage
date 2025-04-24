d89396b838172dcfa091da5402be59904521bf2e
st-thule <thu.le@supremetech.vn>
2025-04-24 17:21:31 +0700

    feat: style for cart

--- src/app/pages/cart/Cart.tsx
+++ src/app/pages/cart/Cart.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 
 import { CartList } from './components/CartList';
 import { CartSummary } from './components/CartSummary';
+import { CartProvider } from '@shared/contexts/CartContext';
 
 export const Cart = () => {
   return (
     <section className="section-cart">
       <div className="container">
         <div className="wrapper">
-          <CartList />
-          <CartSummary />
+          <CartProvider>
+            <CartList />
+            <CartSummary />
+          </CartProvider>
         </div>
       </div>
     </section>
--- src/app/pages/cart/components/CartSummary.tsx
+++ src/app/pages/cart/components/CartSummary.tsx
@@ -2,21 +2,20 @@ import React from 'react';
 
 import Button from '@shared/components/partials/Button';
 import { useCart } from '@shared/contexts/CartContext';
-import { StatusKeys } from '@shared/utils/storage';
 
 export const CartSummary: React.FC = () => {
-  const { totalPrice, totalQuantity, cartStatus } = useCart();
+  const { totalPrice, totalQuantity } = useCart();
   return (
     <div className="cart-summary">
       <h2 className="cart-title">Cart Totals</h2>
       <div className="cart-info">
         <div className="cart-detail">
           <h3 className="cart-label">Quantity</h3>
-          <p className="cart-value">{totalPrice}</p>
+          <p className="cart-value">{totalQuantity}</p>
         </div>
         <div className="cart-detail">
           <h3 className="cart-label">Total</h3>
-          <p className="cart-value">{totalQuantity} VND</p>
+          <p className="cart-value">{totalPrice} VND</p>
         </div>
       </div>
       <div className="cart-action">
